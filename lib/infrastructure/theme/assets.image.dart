47192f6e72caf8ab69b557f9890ef6745970168e
Milzam Hibatullah <hmilzam@gmail.com>
2022-11-01 21:47:41 +0700

    redesign home scree

--- lib/infrastructure/theme/assets.image.dart
+++ lib/infrastructure/theme/assets.image.dart
@@ -6,11 +6,10 @@ class AssetsImage {
       'assets/background/illustration/afternoon.jpg';
   static const String EVENING_ILLUST =
       'assets/background/illustration/evening.jpg';
-  static const String NIGHT_ILLUST =
-      'assets/background/illustration/night.jpg';
+  static const String NIGHT_ILLUST = 'assets/background/illustration/night.jpg';
 
   ///avatars
-  static const String MORNING_AVATAR = 'assets/avatar/morning.jpg';
-  static const String NOON_AVATAR = 'assets/avatar/noon.jpg';
-  static const String NIGHT_AVATAR = 'assets/avatar/night.jpg';
+  static const String MORNING_AVATAR = 'assets/avatar/morning.png';
+  static const String NOON_AVATAR = 'assets/avatar/noon.png';
+  static const String NIGHT_AVATAR = 'assets/avatar/night.png';
 }
--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -1,3 +1,5 @@
+import 'package:Dclock/infrastructure/theme/apps.text.dart';
+import 'package:Dclock/infrastructure/theme/assets.image.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,17 +8,46 @@ import 'controllers/home.controller.dart';
 
 class HomeScreen extends GetView<HomeController> {
   const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeScreen'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
         centerTitle: true,
+        title: appText.title(text: 'Good Night',color: Colors.white),
       ),
-      body: const Center(
-        child: Text(
-          'HomeScreen is working',
-          style: TextStyle(fontSize: 20),
+      extendBodyBehindAppBar: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: const Color(0xFFBAABDA),
+        child: Icon(Icons.add),
+      ),
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AssetsImage.EVENING_ILLUST))),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                height: Get.height/1.3,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                  color: Color(0xFFFDF6EC),
+                  
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
