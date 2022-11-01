044572ce2ec328b4ccea06e8740c5843f0aa7f57
Milzam Hibatullah <hmilzam@gmail.com>
2022-11-01 14:35:49 +0700

    setup assets image

---
+++ lib/infrastructure/theme/assets.image.dart
@@ -0,0 +1,16 @@
+class AssetsImage {
+  ///backgrounds
+  static const String MORNING_ILLUST =
+      '/assets/background/illustration/morning.jpg';
+  static const String AFTERNOON_ILLUST =
+      '/assets/background/illustration/afternoon.jpg';
+  static const String EVENING_ILLUST =
+      '/assets/background/illustration/evening.jpg';
+  static const String NIGHT_ILLUST =
+      '/assets/background/illustration/night.jpg';
+
+  ///avatars
+  static const String MORNING_AVATAR = '/assets/avatar/morning.jpg';
+  static const String NOON_AVATAR = '/assets/avatar/noon.jpg';
+  static const String NIGHT_AVATAR = '/assets/avatar/night.jpg';
+}
