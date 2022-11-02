d439a98159f71e6e17d7f541f50e57a366e5f7a3
Milzam Hibatullah <hmilzam@gmail.com>
2022-11-02 20:42:43 +0700

    initial repeat days on controller

---
+++ lib/domain/core/model/days.model.dart
@@ -0,0 +1,6 @@
+
+class DayModel {
+  final String name;
+  bool selected;
+  DayModel({required this.name,required this.selected});
+}
--- lib/presentation/home/controllers/home.controller.dart
+++ lib/presentation/home/controllers/home.controller.dart
@@ -1,11 +1,48 @@
+import 'package:Dclock/domain/core/model/days.model.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  var days = <DayModel>[].obs;
 
-  final count = 0.obs;
+  @override
+  void onInit() {
+    _setUpDays();
+    super.onInit();
+  }
 
+  _setUpDays() {
+    var tempDays = <DayModel>[];
+    tempDays.add(DayModel(name: 'Min', selected: false));
+    tempDays.add(DayModel(name: 'Sen', selected: false));
+    tempDays.add(DayModel(name: 'Sel', selected: false));
+    tempDays.add(DayModel(name: 'Rab', selected: false));
+    tempDays.add(DayModel(name: 'Kam', selected: false));
+    tempDays.add(DayModel(name: 'Jum', selected: false));
+    tempDays.add(DayModel(name: 'Sab', selected: false));
 
+    days.value=tempDays;
+    days.refresh();
+  }
 
-  void increment() => count.value++;
+  resetDays() {
+    var tempDays = <DayModel>[];
+    tempDays = days.value;
+    for (var element in tempDays) {
+      element.selected = false;
+    }
+
+    days.value = tempDays;
+  }
+
+  selectDays(int index) {
+    var tempDays = <DayModel>[];
+    tempDays = days.value;
+    tempDays[index].selected
+        ? tempDays[index].selected = false
+        : tempDays[index].selected = true;
+
+    days.value=tempDays;
+    days.refresh();
+
+  }
 }
